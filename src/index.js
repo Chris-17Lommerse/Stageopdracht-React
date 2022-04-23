1665c64ea455a993fbd234968a44a1eac95bdaae
Chris-17Lommerse <89691283+Chris-17Lommerse@users.noreply.github.com>
2022-04-23 20:16:53 +0200

    App aangemaakt en alvast een form aangemaakt om data in een object op te slaan
    Dit met de localstorage

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals';
+import useLocalStorage from './useLocalStorage.js';
+
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+ReactDOM.render(element, document.getElementById('root'));
+var element = React.createElement < "input" >
+    reportWebVitals();
