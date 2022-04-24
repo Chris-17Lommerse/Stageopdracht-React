5976716904de06d438a61f207c1f193fcfe1878c
Chris-17Lommerse <89691283+Chris-17Lommerse@users.noreply.github.com>
2022-04-24 13:09:11 +0200

    Test om iets op de webpagina te krijgen
    qua tekst

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import useLocalStorage from './useLocalStorage.js';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+var element = React.createElement('h1', { className: 'greeting' }, 'React Test App');
 ReactDOM.render(element, document.getElementById('root'));
 var element = React.createElement < "input" >
     reportWebVitals();
