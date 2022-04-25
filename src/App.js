43bb2eb45c5dbdb37e56ae3d9c11dfc002eaaa7e
Chris-17Lommerse <89691283+Chris-17Lommerse@users.noreply.github.com>
2022-04-25 13:48:05 +0200

    Proberen om een api te gebruiken met react
    Ik ben aan het proberen om een api te gebruiken voor mijn react applicatie ( nog niet gelukt ). Ook ben ik bezig om de form in de app te laten zien ( helaas krijg ik dat ook niet voor elkaar )

--- src/App.js
+++ src/App.js
@@ -1,19 +1,70 @@
 import './App.css';
 import React, { useState } from 'react';
 import useLocalStorage from './useLocalStorage';
+import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+// constructor(props); {
+//     super(props);
+
+//     this.state = {
+//         item: [],
+//         DataisLoaded: false
+//     };
+// }
 
 
-function App() {
+// componentDidMoumt(); {
+//     fetch(
+//         "https://jsonplaceholder.typicode.com/users")
+//         .then((res) => res.json()
+//         .then((json) => {
+//             this.setState({
+//                 items: json,
+//                 DataisLoaded: true
+//             });
+//         }))
+// }
+//     render(); {
+//         const {DataisLoaded, items} = this.state;
+//         if (!DataisLoaded) return <div>
+//             <h1> Please wait some time... </h1> </div> ;
+//         }
+//             return (
+//                 <div className= "App">
+//                     <h1> Fetch data from an api in react </h1> {
+//                         items.map((item) => (
+//                             <ol key = { item.id } >
+//                                 User_Name: {item.username},
+//                                 Full_Name: {item.name},
+//                                 User_Email: {item.email}
+//                             </ol>
+//                         ))
+//                       }
+//                 </div>
+//             );
+//         }
 
-    let [value, setValue] = useLocalStorage('name', '');
+export default App;
+
+function Form() {
+
+    const [name, setName] = useState("");
 
     return ( <
+        form >
+        <
+        label > Enter your name:
+        <
         input type = "text"
+        value = { name }
         onChange = {
-            (event) => setValue(event.target.value)
-        }
-        value = { value }
-        />
-    );
+            (e) => setName(e.target.value) }
+        /> 
+        </label> 
+        </form>
+    )
 }
-export default App;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( < Form / > );
--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import useLocalStorage from './useLocalStorage.js';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'React Test App');
+var element = React.createElement('h1', { className: 'greeting' }, 'React ', 'Test ', 'App ');
 ReactDOM.render(element, document.getElementById('root'));
-var element = React.createElement < "input" >
-    reportWebVitals();
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
